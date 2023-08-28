75188e00054cb6f9dcbf5b68d45ee13a901c3963
veeranki2014 <veeranki2014@gmail.com>
2023-08-27 19:12:09 -0500

    aws-terraform

--- main.tf
+++ main.tf
@@ -7,16 +7,19 @@ resource "aws_ssm_parameter" "param" {
 
 variable "params" {
   default = [
-    { name = "roboshop.dev.frontend.catalogue_url", value = "http://catalogue-dev.veerankitek.com/", type = "String" },
-    { name = "roboshop.dev.frontend.user_url", value = "http://user-dev.veerankitek.com/", type = "String" },
-    { name = "roboshop.dev.frontend.cart_url", value = "http://cart-dev.veerankitek.com/", type = "String" },
-    { name = "roboshop.dev.frontend.payment_url", value = "http://payment-dev.veerankitek.com/", type = "String" },
-    { name = "roboshop.dev.frontend.shipping_url", value = "http://shipping-dev.veerankitek.com/", type = "String" },
+    { name = "roboshop.dev.frontend.catalogue_url", value = "http://catalogue-dev.veerankitek.com:8080/", type = "String" },
+    { name = "roboshop.dev.frontend.user_url", value = "http://user-dev.veerankitek.com:8080/", type = "String" },
+    { name = "roboshop.dev.frontend.cart_url", value = "http://cart-dev.veerankitek.com:8080/", type = "String" },
+    { name = "roboshop.dev.frontend.payment_url", value = "http://payment-dev.veerankitek.com:8080/", type = "String" },
+    { name = "roboshop.dev.frontend.shipping_url", value = "http://shipping-dev.veerankitek.com:8080/", type = "String" },
+
     { name = "roboshop.dev.cart.redis_host", value = "redis-dev.veerankitek.com", type = "String" },
     { name = "roboshop.dev.cart.catalogue_host", value = "catalogue-dev.veerankitek.com", type = "String" },
     { name = "roboshop.dev.cart.catalogue_port", value = "8080", type = "String" },
+
     { name = "roboshop.dev.catalogue.mongo", value = "true", type = "String" },
     { name = "roboshop.dev.catalogue.mongo_url", value = "mongodb://mongodb-dev.veerankitek.com:27017/catalogue", type = "String" },
+
     { name = "roboshop.dev.payment.cart_host", value = "cart-dev.veerankitek.com", type = "String" },
     { name = "roboshop.dev.payment.cart_port", value = "8080", type = "String" },
     { name = "roboshop.dev.payment.user_host", value = "user-dev.veerankitek.com", type = "String" },
@@ -29,6 +32,7 @@ variable "params" {
 
     { name = "roboshop.dev.shipping.cart_endpoint", value = "cart-dev.veerankitek.com:8080", type = "String" },
     { name = "roboshop.dev.shipping.db_host", value = "mysql-dev.veerankitek.com", type = "String" },
+
     { name = "roboshop.dev.user.mongo", value = "true", type = "String" },
     { name = "roboshop.dev.user.redis_host", value = "redis-dev.veerankitek.com", type = "String" },
     { name = "roboshop.dev.user.mongo_url", value = "mongodb://mongodb-dev.veerankitek.com:27017/users", type = "String" }
